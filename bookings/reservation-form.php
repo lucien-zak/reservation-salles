7760442a30fc84c2878b3a697d2b30815312cea3
Kilian Grn <aeroxdev@gmail.com>
2021-12-20 17:25:43 +0100

    Fix

--- bookings/reservation-form.php
+++ bookings/reservation-form.php
@@ -5,6 +5,10 @@ if (!$_SESSION) {
     header("location:./planning.php");
 }
 
+if(!isset($_GET['heure'])) {
+    $_GET['heure'] = "08";
+}
+
 if(isset($_POST['submit'])) {
     $titre = $_POST['titre'];
     $description = $_POST['description'];
@@ -21,26 +25,40 @@ if(isset($_POST['submit'])) {
 <body>
     <?php require('../src/header.php'); ?>
     <main>
-        <form action="" method="post">
-            <label for="titre">Titre :</label>
-            <input name="titre" type="text" />
-            <textarea name="description" cols="30" rows="7">Description :</textarea>
-            <input name="date" type="date"  value="<?= $_GET['date'] ?>" min="<?= date('Y-m-d') ?>" >
-            <select name="starthour">
-                <option value="sh">Heure de début</option>
-                <?php 
-                for($i = 8; $i <= 19; $i++ ) 
-                {
-                    if ($i < 10) {
-                        ?><option <?= $i == $_GET['heure'] ? "selected" : "" ?> value="0<?=$i?>">0<?=$i?>h00</option><?php                
-                    } elseif ($i >= 10) {
-                        ?><option <?= $i == $_GET['heure'] ? "selected" : "" ?> value="<?=$i?>"><?=$i?>h00</option><?php                
-                    }
-                }?>
-            </select>
-            <input type="submit" name="submit">
-        </form>
+        <div class="top">
+            <img src="../assets/img/blob-1.svg" class="img-shape" alt="">
+            <form class="reservation-form" action="" method="post">
+                <h2>Réservation</h2>
+                <input name="titre" class="entry" type="text" placeholder="Nom de votre évènement" />
+                <textarea name="description" class="box-entry" placeholder="Détaillé votre évènement" cols="30" rows="7"></textarea>
+                <input name="date" type="date" class="entry" value="<?= $_GET['date'] ?>" min="<?= date('Y-m-d') ?>" >
+                <select name="starthour" class="select-entry">
+                    <option value="sh">Heure de début</option>
+                    <?php 
+                    for($i = 8; $i <= 19; $i++ ) 
+                    {
+                        if ($i < 10) {
+                            ?><option <?= $i == $_GET['heure'] ? "selected" : "" ?> value="0<?=$i?>">0<?=$i?>h00</option><?php                
+                        } elseif ($i >= 10) {
+                            ?><option <?= $i == $_GET['heure'] ? "selected" : "" ?> value="<?=$i?>"><?=$i?>h00</option><?php                
+                        }
+                    }?>
+                </select>
+                <select name="endhour" class="select-entry">
+                    <option value="sh">Heure de début</option>
+                    <?php 
+                    for($i = 8; $i <= 19; $i++ ) 
+                    {
+                        if ($i < 10) {
+                            ?><option <?= $i == $_GET['heure'] ? "selected" : "" ?> value="0<?=$i?>">0<?=$i?>h00</option><?php                
+                        } elseif ($i >= 10) {
+                            ?><option <?= $i == $_GET['heure'] ? "selected" : "" ?> value="<?=$i?>"><?=$i?>h00</option><?php                
+                        }
+                    }?>
+                </select>
+                <input type="submit" class="entry" name="submit">
+            </form>
+        </div>
     </main>
 </body>
 </html>
-
