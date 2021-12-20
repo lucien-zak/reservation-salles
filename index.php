a087e863a4711ccef98478dffcd4ea571af511f4
Kilian Grn <aeroxdev@gmail.com>
2021-12-20 09:50:36 +0100

    Modif de plusieurs élements ajout de materials.

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    require('./config.php')
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,8 +9,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 <body>
-    
+    <?php
+    $namePage = "Home";
+    require('./src/header.php'); 
+    ?>
+    <main>
+        <div class="contener">
+                <h1>ReserveTaSalle.fr</h1>
+                <h3 style="text-align: center;">Réserver la salle de vos rêves dans votre ville, la réservation se fait simplement en remplissant un formulaire, 
+                <br>après cela un suivi de votre évènement est disponible.</h3>
+        </div>
+    </main>    
 </body>
 </html>
--- src/header.php
+++ src/header.php
@@ -1,15 +1,28 @@
 <header>
-    <?php if($_SESSION) { ?>
-        <a href="../index.php">Home</a>
-        <a href="../bookings/planning.php">Planning</a>
+    <?php if($_SESSION) { 
+        // Gauche
+        if($GLOBALS['namePage'] == "Home") {
+            echo '<a href="/reservation-salles/users/profil.php">Profil</a>';
+        } else {
+            echo '<a href="/reservation-salles/index.php">Home</a>';
+        } ?>
+        <a href="/reservation-salles/bookings/planning.php">Planning</a>
+        <!-- Centre -->
         <h1><?= $GLOBALS['namePage'] ?></h1>
-        <a href="../bookings/reservation-form.php">Réserver</a>
-        <a href="../bookings/reservation.php">Réservation</a>
-    <?php } else { ?>
-        <a href="../index.php">Home</a>
-        <a href="../bookings/planning.php">Planning</a>
-        <h1><?= $GLOBALS['namePage'] ?></h1>
-        <a href="../users/login.php">Login</a>
-        <a href="../users/register.php">Register</a>
+        <!-- Droite -->
+        <a>En savoirs plus</a>
+        <a href="/reservation-salles/src/logout.php">Logout</a>
+    <?php } else { 
+        if($GLOBALS['namePage'] == 'Home') {
+            echo '<h1>Home</h1>';
+        } else {
+            echo '<a href="/reservation-salles/index.php">Home</a>';
+        }?>
+        <a href="/reservation-salles/bookings/planning.php">Planning</a>
+        <!-- Centre -->
+        <!-- <h1><?= $GLOBALS['namePage'] ?></h1> -->
+        <!-- Droite -->
+        <a href="/reservation-salles/users/login.php">Login</a>
+        <a href="/reservation-salles/users/register.php">Register</a>
     <?php } ?>
 </header>
